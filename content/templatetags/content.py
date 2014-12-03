9c501314677f542a74c36e1d3d114d61e314d9e9
Giacomo Graziosi <g.graziosi@gmail.com>
2014-12-03 11:52:05 +0100

    random item templatetag

--- content/templatetags/content.py
+++ content/templatetags/content.py
@@ -9,3 +9,8 @@ register = template.Library()
 @register.assignment_tag()
 def content(slug):
     return Content.objects.get(translations__slug=slug)
+
+
+@register.assignment_tag()
+def random_item(items):
+    return items.order_by('?').first()
